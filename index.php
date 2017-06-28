42b6641635e028aa472860eecd6e32abd6b5fe26
avenirs05 <avenirs05@gmail.com>
2017-06-28 19:04:06 +0300

    mobile gallery done

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 ﻿<!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
-    <meta name="format-detection" content="telephone=no" />
+    <meta name="format-detection" content="telephone=no">
 	
     <title>Veter61</title>
     
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
     <link rel="shortcut icon" href="/imgs/fav.png" type="image/png">
-    <link type="text/css" rel="stylesheet" href="css/lightGallery.css" />
+    <link type="text/css" rel="stylesheet" href="css/lightGallery.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">    
     <link href="style.css" rel="stylesheet">
 
@@ -19,22 +20,18 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/lightgallery.min.js"></script>
     <script src="js/lg-thumbnail.min.js"></script>
-    <!-- lightgallery plugins -->
-<!--     <script src="js/lg-thumbnail.min.js"></script>
-    <script src="js/lg-fullscreen.min.js"></script> -->
 
     <script>
         $(function () {
-            $('.carousel').carousel();
+            $('#carousel-main-screen').carousel();
+            $('#carousel-gallery-mob').carousel();
             $("#lightgallery").lightGallery( {
                 showThumbByDefault: true
             });
         });
-
     </script>
-<!--     <script src="events-general.js"></script> -->
-
 </head>
+
 <body>
 
 <nav class="navbar navbar-default navbar-fixed-top visible-md-block visible-lg-block">
@@ -69,13 +66,13 @@
     </div>
 </nav>
 
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-main-screen" class="carousel slide" data-ride="carousel">
     <h1 class="text-center">Квартиры от застройщика в новом доме в 10 минутах от центра города!</h1>
   <!-- Indicators -->
     <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-main-screen" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-main-screen" data-slide-to="1"></li>
+        <li data-target="#carousel-main-screen" data-slide-to="2"></li>
     </ol>
   <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
@@ -90,11 +87,11 @@
         </div>               
     </div>
   <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <a class="left carousel-control" href="#carousel-main-screen" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <a class="right carousel-control" href="#carousel-main-screen" role="button" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
@@ -105,7 +102,7 @@
         <div class="col-md-12">
             <p>Жилой дом «Ветер перемен» – это 8-этажный 42-квартирный дом клубного типа, расположенный на тихой улочке в Ленинском районе г. Ростова-на-Дону.</p>
             <p>Концепция дома – уютный и тихий анклав для своих. Микрорайон «Новое поселение» - один из немногих, где отсутствует масштабная многоэтажная застройка.</p>
-            <p class="attention"><span class="red">Внимание!</span> Мы не предлагаем долевое участие. Дом построен и сдан в эксплуатацию. Вы получаете квартиры в готовом доме с зарегистрированным правом собственности!</p>
+            <p class="attention text-center"><span class="red">Внимание!</span> Мы не предлагаем долевое участие. Дом построен и сдан в эксплуатацию. Вы получаете квартиры в готовом доме с зарегистрированным правом собственности!</p>
             <p class="bold">Ищем порядочных и умных соседей!</p>
             <p>Всего по 6 квартир на каждом этаже. Многие будущие жильцы, купившие здесь квартиры, знакомы друг с другом. Руководители  компаний застройщика и генподрядчика приняли решение оставить несколько квартир для себя. Безупречное качество строительства – один из главных аргументов при выборе квартиры в нашем жилом доме.</p>
             <p>Наш дом не похож на другие. В нем есть то, чего нет у большинства ростовских многоэтажек, - душá. Уютный дворик создает ощущение домашней террасы, а при входе в подъезд тихо играет легкая классическая музыка.</p>
@@ -141,7 +138,7 @@
             <p>Квартиры продаются в состоянии «стройвариант». При желании предоставляются услуги дизайна интерьера и ремонта «под ключ».</p>
             <p>В доме имеются 1- и 2-комнатные квартиры площадью от 35,1 кв.м до 71,1 кв.м. При этом возможно объединение квартир на выгодных условиях. Также в продаже нежилые помещения и машино-места в подземном паркинге. Количество квартир и машино-мест ограничено.</p>
             <p>Запись на просмотр по телефону: 8(863) 296-02-60 (без выходных).</p>
-            <p class="attention"><span class="red">Внимание!</span> Дом построен в строгом соответствии с проектом, прошедшим экспертизу, и разрешением на строительство. Никаких «самозастроев» и «долевых» квартир!</p>
+            <p class="attention text-center"><span class="red">Внимание!</span> Дом построен в строгом соответствии с проектом, прошедшим экспертизу, и разрешением на строительство. Никаких «самозастроев» и «долевых» квартир!</p>
             <p>Жилой дом «Ветер перемен»: взгляните по-новому на городскую жизнь!</p>
             <p>Настало время для перемен к лучшему!</p>
         </div>
@@ -151,7 +148,7 @@
 <div class="container-fluid gallery visible-md-block visible-lg-block">
     <div class="row">
         <div class="col-md-12 text-center">
-            <h2 class="text-center">Галерея</h2>            
+            <h2 class="text-center">Фотогалерея</h2>            
             <div id="lightgallery">
               <a href="imgs/gallery-01.jpg">
                   <img src="imgs/gallery-thumb-01.jpg">
@@ -244,6 +241,56 @@
         </div>
     </div>
 </div>
+
+<div class="container-fluid gallery-mob visible-xs-block visible-sm-block"> 
+    <h2 class="text-center">Фотогалерея</h2>           
+    <div id="carousel-gallery-mob" data-ride="carousel" class="carousel slide">
+        <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+              <div id="gal-pict-01" class="item active"></div>
+              <div id="gal-pict-02" class="item"></div>
+              <div id="gal-pict-03" class="item"></div>
+              <div id="gal-pict-04" class="item"></div>
+              <div id="gal-pict-05" class="item"></div>
+              <div id="gal-pict-06" class="item"></div>
+              <div id="gal-pict-07" class="item"></div>
+              <div id="gal-pict-08" class="item"></div>
+              <div id="gal-pict-09" class="item"></div>  
+              <div id="gal-pict-10" class="item"></div>   
+              <div id="gal-pict-11" class="item"></div>
+              <div id="gal-pict-12" class="item"></div>
+              <div id="gal-pict-13" class="item"></div>
+              <div id="gal-pict-14" class="item"></div>
+              <div id="gal-pict-15" class="item"></div>
+              <div id="gal-pict-16" class="item"></div>
+              <div id="gal-pict-17" class="item"></div>
+              <div id="gal-pict-18" class="item"></div>
+              <div id="gal-pict-19" class="item"></div>  
+              <div id="gal-pict-20" class="item"></div>
+              <div id="gal-pict-21" class="item"></div>
+              <div id="gal-pict-22" class="item"></div>
+              <div id="gal-pict-23" class="item"></div>
+              <div id="gal-pict-24" class="item"></div>
+              <div id="gal-pict-25" class="item"></div>
+              <div id="gal-pict-26" class="item"></div>
+              <div id="gal-pict-27" class="item"></div>
+              <div id="gal-pict-28" class="item"></div>
+              <div id="gal-pict-29" class="item"></div>                 
+          </div>
+        <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-gallery-mob" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-gallery-mob" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+          </a>
+    </div>
+</div>
+
+
+
 
 
 <!-- <div class="main-screen"> -->
