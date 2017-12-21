0fe2ff57ea4ac6497f66234576d01884d6c98639
avenirs05 <avenirs05@gmail.com>
2017-12-21 15:49:19 +0200

    last

--- floors/floor-3.php
+++ floors/floor-3.php
@@ -1,10 +1,10 @@
 ﻿<a class="left-arrow" href="/plans.php?floor=2"><</a>
 <a class="right-arrow" href="/plans.php?floor=4">></a>
 <div class="text-center">
-    <a href="#" id="flat-orange" class="floor-3 flat-plan-cut sold"></a><a href="#" id="flat-blue" class="floor-3 flat-plan-cut sold" href="return false"></a><div href="#" id="hall" class="flat-plan-cut"></div><a href="#" id="flat-turq" class="floor-3 flat-plan-cut"></a>
+    <a href="#" id="flat-orange" class="floor-3 flat-plan-cut sold"></a><a href="#" id="flat-blue" class="floor-3 flat-plan-cut sold" href="return false"></a><div href="#" id="hall" class="flat-plan-cut"></div><a href="#" id="flat-turq" class="floor-3 flat-plan-cut reserved"></a>
 </div>
 <div id="second-row-wrap" class="text-center">
-    <a href="#" id="flat-green" class="floor-3 flat-plan-cut"></a><a href="#" id="flat-violet" class="floor-3 flat-plan-cut sold"></a><a href="#" id="flat-yellow" class="floor-3 flat-plan-cut"></a><a href="#" id="flat-turq2" class="floor-3 flat-plan-cut"><div id="fake-green-div"></div></a>
+    <a href="#" id="flat-green" class="floor-3 flat-plan-cut reserved"></a><a href="#" id="flat-violet" class="floor-3 flat-plan-cut sold"></a><a href="#" id="flat-yellow" class="floor-3 flat-plan-cut sold"></a><a href="#" id="flat-turq2" class="floor-3 flat-plan-cut"><div id="fake-green-div"></div></a>
 </div>
 
 
