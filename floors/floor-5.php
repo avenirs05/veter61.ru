150b8f160493cf3c01f1090afeb98936bfeb05ae
avenirs05 <avenirs05@gmail.com>
2017-12-29 19:46:13 +0200

    last

--- floors/floor-5.php
+++ floors/floor-5.php
@@ -4,7 +4,7 @@
     <a href="#" id="flat-orange" class="floor-5 flat-plan-cut sold"></a><a href="#" id="flat-blue" class="floor-5 flat-plan-cut" href="return false"></a><div href="#" id="hall" class="flat-plan-cut"></div><a href="#" id="flat-turq" class="floor-5 flat-plan-cut"></a>
 </div>
 <div id="second-row-wrap" class="text-center">
-    <a href="#" id="flat-green" class="floor-5 flat-plan-cut sold"></a><a href="#" id="flat-violet" class="floor-5 flat-plan-cut sold"></a><a href="#" id="flat-yellow" class="floor-5 flat-plan-cut reserved"></a><a href="#" id="flat-turq2" class="floor-5 flat-plan-cut"><div id="fake-green-div"></div></a>
+    <a href="#" id="flat-green" class="floor-5 flat-plan-cut sold"></a><a href="#" id="flat-violet" class="floor-5 flat-plan-cut sold"></a><a href="#" id="flat-yellow" class="floor-5 flat-plan-cut sold"></a><a href="#" id="flat-turq2" class="floor-5 flat-plan-cut"><div id="fake-green-div"></div></a>
 </div>
 
 
