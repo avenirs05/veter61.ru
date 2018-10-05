2a7dd148827544bd0150983e3e1e4b4a98157baa
avenirs05 <avenirs05@gmail.com>
2018-10-05 19:18:56 +0300

    last

--- floors/floor-7.php
+++ floors/floor-7.php
@@ -1,7 +1,7 @@
 ﻿<a class="left-arrow" href="/plans.php?floor=6"><</a>
 <a class="right-arrow" href="/plans.php?floor=8">></a>
 <div class="text-center">
-    <a href="#" id="flat-orange" class="floor-7 flat-plan-cut sold"></a><a href="#" id="flat-blue" class="floor-7 flat-plan-cut" href="return false"></a><div href="#" id="hall" class="flat-plan-cut"></div><a href="#" id="flat-turq" class="floor-7 flat-plan-cut"></a>
+    <a href="#" id="flat-orange" class="floor-7 flat-plan-cut sold"></a><a href="#" id="flat-blue" class="floor-7 flat-plan-cut sold" href="return false"></a><div href="#" id="hall" class="flat-plan-cut"></div><a href="#" id="flat-turq" class="floor-7 flat-plan-cut"></a>
 </div>
 <div id="second-row-wrap" class="text-center">
     <a href="#" id="flat-green" class="floor-7 flat-plan-cut sold"></a><a href="#" id="flat-violet" class="floor-7 flat-plan-cut sold"></a><a href="#" id="flat-yellow" class="floor-7 flat-plan-cut sold"></a><a href="#" id="flat-turq2" class="floor-7 flat-plan-cut"><div id="fake-green-div"></div></a>
--- floors/floor-8.php
+++ floors/floor-8.php
@@ -3,7 +3,7 @@
     <a href="#" id="flat-orange" class="floor-8 flat-plan-cut sold"></a><a href="#" id="flat-blue" class="floor-8 flat-plan-cut sold" href="return false"></a><div href="#" id="hall" class="flat-plan-cut"></div><a href="#" id="flat-turq" class="floor-8 flat-plan-cut"></a>
 </div>
 <div id="second-row-wrap" class="text-center">
-    <a href="#" id="flat-green" class="floor-8 flat-plan-cut"></a><a href="#" id="flat-violet" class="floor-8 flat-plan-cut"></a><a href="#" id="flat-yellow" class="floor-8 flat-plan-cut reserved"></a><a href="#" id="flat-turq2" class="floor-8 flat-plan-cut"><div id="fake-green-div"></div></a>
+    <a href="#" id="flat-green" class="floor-8 flat-plan-cut"></a><a href="#" id="flat-violet" class="floor-8 flat-plan-cut"></a><a href="#" id="flat-yellow" class="floor-8 flat-plan-cut sold"></a><a href="#" id="flat-turq2" class="floor-8 flat-plan-cut"><div id="fake-green-div"></div></a>
 </div>
 
 
